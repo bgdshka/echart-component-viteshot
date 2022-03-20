32f5722a85e8f1baa932d96306c8ba4eff719846
Kirill Ateev <kirill.ateev59@gmail.com>
2022-03-20 13:02:26 +0300

    feat: add devices tests

--- viteshot.config.js
+++ viteshot.config.js
@@ -5,6 +5,17 @@ module.exports = {
   framework: {
     type: "react",
   },
-  shooter: playwrightShooter(playwright.chromium),
+  shooter: playwrightShooter(playwright.chromium, {
+    // Take each screenshot in two different viewports.
+    contexts: {
+      laptop: {
+        viewport: {
+          width: 1366,
+          height: 768,
+        },
+      },
+      pixel2: playwright.devices["Pixel 2"],
+    }
+  }),
   filePathPattern: "**/*.screenshot.@(js|jsx|tsx|vue|svelte)",
 };
